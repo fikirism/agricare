d0d2900fd9e604e276c4dae9499994c0e323b2ee
FIKIRI SILVIN MATATIZO <100496931+fikirism@users.noreply.github.com>
2023-10-21 16:25:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from torch.functional import split
-from model_files.ml_predict import predict_plant, Network
+from model_files.prediction import predict_plant, Network
 from pyfcm import FCMNotification
 import base64
 from decouple import config
 
 
-app = Flask("Plant Disease Detector")
+app = Flask("Bean Disease Detector")
 CORS(app)
 push_service = FCMNotification(api_key=config('API_KEY'))
 
